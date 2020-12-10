bc736614d31bb1489e06f344accbad03c33760c5
Minori Hashimoto <minori.fh@gmail.com>
2020-12-10 13:25:38 -0800

    edit express.static

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(cors())
 app.use(routes)
 
 if (process.env.NODE_ENV === "production"){
-    app.use(express.static("client/build"));; // this folder will not exist until "npm run build" is run for the first time in the client
+    app.use(express.static(__dirname + '/client/build')); // this folder will not exist until "npm run build" is run for the first time in the client
     
     app.get('*', function(req, res) {
         res.sendFile(path.join(__dirname, 'client/build/index.html'));
