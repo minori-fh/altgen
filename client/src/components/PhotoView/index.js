341eca8a6f94b5907c56c735233fb4617fd3d1e6
Minori Hashimoto <minori.fh@gmail.com>
2021-02-06 16:53:08 -0800

    edit grid styling

--- client/src/components/PhotoView/index.js
+++ client/src/components/PhotoView/index.js
@@ -18,8 +18,11 @@ function PhotoView(props){
         return(
         <div className="jumbo-wrapper">
             <img className="img-jumbo" src={urls[file]}/>
-            <div>{file}</div>
-            <CopyBtn text={alt[file]}/>
+            <div className="sub">
+                <div id="filename">{file}</div>
+                <CopyBtn text={alt[file]}/>
+            </div>
+
         </div>)
     })
 
